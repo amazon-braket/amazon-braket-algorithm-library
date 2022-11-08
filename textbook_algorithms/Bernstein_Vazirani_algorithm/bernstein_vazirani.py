a7581a9e0cc3579215a26fa014ba815f9bc09f23
Matt Beach <drbeach@amazon.com>
2022-11-08 14:31:13 -0500

    BV algorithm 1st draft

---
+++ textbook_algorithms/Bernstein_Vazirani_algorithm/bernstein_vazirani.py
@@ -0,0 +1,67 @@
+from collections import Counter
+
+import matplotlib.pyplot as plt
+from braket.circuits import Circuit
+
+
+def bernstein_vazirani_circuit(hidden_string: str = "011") -> Circuit:
+    """Bernstein–Vazirani circuit on a hidden string.
+
+    Args:
+        num_qubits (int, optional): _description_. Defaults to 3.
+        hidden_string (str, optional): _description_. Defaults to "011".
+
+    Returns:
+        Circuit: Bernstein–Vazirani circuit
+    """
+
+    num_qubits = len(hidden_string)
+
+    bv_circuit = Circuit()
+    bv_circuit.h(num_qubits)
+    bv_circuit.z(num_qubits)
+    bv_circuit.h(range(num_qubits))
+
+    for q in range(num_qubits):
+        if hidden_string[q] == "0":
+            bv_circuit.i(q)
+        else:
+            bv_circuit.cnot(q, num_qubits)
+
+    bv_circuit.h(range(num_qubits))
+
+    for i in range(num_qubits):
+        bv_circuit.probability(i)
+    return bv_circuit
+
+
+def marginalize_measurements(measurement_counts: Counter) -> Counter:
+    """Remove the last qubit measurement counts.
+
+    Args:
+        measurement_counts (Counter): Measurement counts from circuit.
+
+    Returns:
+        Counter: Measurement counts from without the last qubit.
+    """
+    new_dict = {}
+    for k, v in measurement_counts.items():
+        if k[:-1] not in new_dict:
+            new_dict[k[:-1]] = v
+        else:
+            new_dict[k[:-1]] += v
+    return Counter(new_dict)
+
+
+def plot_bitstrings(counts: Counter, title: str = None):
+    """Plot the measure results
+
+    Args:
+        counts (Counter): Measurement counts.
+        title (str): Title for the plot.
+    """
+    plt.bar(counts.keys(), counts.values())
+    plt.xlabel("bitstrings")
+    plt.ylabel("counts")
+    plt.title(title)
+    plt.xticks(rotation=90)
