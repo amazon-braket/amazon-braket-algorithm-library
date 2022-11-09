2f226da3cd214b3df8322331a60502b5c4cc109f
Matt Beach <drbeach@amazon.com>
2022-11-09 15:35:16 -0500

    add test

--- textbook_algorithms/Bells_Inequality/test_bells_inequality.py
+++ textbook_algorithms/Bells_Inequality/test_bells_inequality.py
@@ -7,7 +7,8 @@ from braket.devices import LocalSimulator
 def test_bell():
     local_simulator = LocalSimulator()
     local_tasks = submit_bell_tasks(local_simulator, shots=0)
-    _, pAB, pAC, pBC = get_bell_results(local_tasks)
+    results, pAB, pAC, pBC = get_bell_results(local_tasks)
     assert math.isclose(pAB, -0.5)
     assert math.isclose(pBC, -0.5)
     assert math.isclose(pAC, 0.5)
+    assert len(results) == 3
