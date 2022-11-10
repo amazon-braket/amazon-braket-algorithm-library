b824f0b7b93d30ca1d1835b1ec1fe75afbae18b2
Mao Lin <maonlinml@amazon.com>
2022-11-10 12:31:48 -0800

    add quantum walk example

---
+++ quantum_walk.py
@@ -0,0 +1,187 @@
+import numpy as np
+
+from braket.circuits import Circuit
+from braket.devices import Device
+from typing import Any, Dict
+
+from collections import Counter
+import matplotlib.pyplot as plt
+
+
+def qft(num_qubits: int, inverse:bool=False) -> Circuit:
+    """
+    Creates the quantum Fourier transform circuit and its inverse
+
+    Args:
+        num_qubits (int): Number of qubits in the circuit
+        inverse (bool): If true return the inverse of the circuit. Default is False
+
+    Returns:
+        Circuit: Circuit object that implements the quantum Fourier transform or its inverse
+    """
+
+    qc = Circuit()
+    N = num_qubits - 1
+    
+    if inverse == False:
+        qc.h(N)
+        for n in range(1, N+1):
+            qc.cphaseshift(N-n, N, 2*np.pi / 2**(n+1))
+
+        for i in range(1, N):
+            qc.h(N-i)
+            for n in range(1, N-i+1):
+                qc.cphaseshift(N-(n+i), N-i, 2*np.pi / 2**(n+1))
+        qc.h(0)
+    
+    else: # The inverse of the quantum Fourier transform
+        qc.h(0)
+        for i in range(N-1, 0, -1):
+            for n in range(N-i, 0, -1):
+                qc.cphaseshift(N-(n+i), N-i, -2*np.pi / 2**(n+1))
+            qc.h(N-i)
+                
+        for n in range(N, 0, -1):
+            qc.cphaseshift(N-n, N, -2*np.pi / 2**(n+1))
+                
+        qc.h(N)
+        
+    return qc
+
+def qft_conditional_add_1(num_qubits:int):
+    """
+    Creates the quantum circuit that conditionally add +1 or -1 using
+    1) The first qubit to control if add 1 or subtract 1: when the first qubit
+        is 0, we add 1 from the number, and when the first qubit
+        is 1, we subtract 1 from the number.
+    2) The second register with `num_qubits` qubits to save the result
+
+    Args:
+        num_qubits (int): Number of qubits that saves the result.
+
+    Returns:
+        Circuit: Circuit object that implements the circuit that conditionally add +1 or -1
+    """
+
+    qc = Circuit()
+    qc.add(qft(num_qubits), target=range(1, num_qubits+1))
+
+    # add \pm 1 with control phase gates
+    for i in range(num_qubits):
+        qc.cphaseshift01(control=0, target=num_qubits-i, angle=2*np.pi / 2**(num_qubits-i))
+        qc.cphaseshift(control=0, target=num_qubits-i, angle=-2*np.pi / 2**(num_qubits-i))
+
+    qc.add(qft(num_qubits, inverse=True), target=range(1, num_qubits+1))
+
+    return qc
+
+def quantum_walk(n_nodes: int, num_steps: int = 1) -> Circuit:
+    """
+    Creates the quantum random walk circuit
+
+    Args:
+        n_nodes (int): The number of nodes in the graph
+        num_steps (int): The number of steps for the quantum walk. Default is 1
+
+    Returns:
+        Circuit: Circuit object that implements the quantum random walk algorithm
+
+    Raises:
+        If `np.log2(n_nodes)` is not an integer, a value error will be raised. 
+    """
+
+    n = np.log2(n_nodes) # number of qubits for the graph
+
+    if float(n).is_integer():
+        n = int(n)
+    else:
+        raise ValueError("The number of nodes has to be 2^n for integer n.")
+    
+    qc = Circuit()
+    for _ in range(num_steps):
+        qc.h(0)
+        qc.add_circuit(qft_conditional_add_1(n))
+    
+    return qc
+
+
+
+def run_quantum_walk(
+    circ: Circuit,
+    device: Device,
+    shots: int = 1000,
+) -> Dict[str, Any]:
+    """
+    Function to run quantum random walk algorithm and return measurement counts.
+
+    Args:
+        circ (Circuit): Quantum random walk circuit
+        device (Device): Braket device backend
+        shots (int) : Number of measurement shots (default is 1000).
+                      0 shots results in no measurement.
+
+    Returns:
+        dict: measurements and results from running Quantum Phase Estimation
+    """
+
+    # Add desired results_types
+    circ.probability()
+
+    # get total number of qubits
+    num_qubits = circ.qubit_count
+
+    # Run the circuit with all zeros input.
+    # The query_circuit subcircuit generates the desired input from all zeros.
+    task = device.run(circ, shots=shots)
+
+    # get result for this task
+    result = task.result()
+
+    # get metadata
+    metadata = result.task_metadata
+
+    # get output probabilities (see result_types above)
+    probs_values = result.values[0]
+
+    # get measurement results
+    measurements = result.measurements
+    measured_qubits = result.measured_qubits
+    measurement_counts = result.measurement_counts
+    measurement_probabilities = result.measurement_probabilities
+
+    # bitstrings
+    format_bitstring = "{0:0" + str(num_qubits) + "b}"
+    bitstring_keys = [format_bitstring.format(ii) for ii in range(2**num_qubits)]
+
+    # 
+    quantum_walk_measurement_counts = {}
+    for key, val in measurement_counts.items():
+        quantum_walk_measurement_counts[int(key[1:], 2)] = val/shots
+    # aggregate results
+    out = {
+        "circuit": circ,
+        "task_metadata": metadata,
+        "measurements": measurements,
+        "measured_qubits": measured_qubits,
+        "measurement_counts": measurement_counts,
+        "measurement_probabilities": measurement_probabilities,
+        "probs_values": probs_values,
+        "bitstring_keys": bitstring_keys,
+        "quantum_walk_measurement_counts": quantum_walk_measurement_counts,
+    }
+
+    return out
+
+
+
+def plot_bitstrings(counts: Counter, title: str = None):
+    """Plot the measure results
+    Args:
+        counts (Counter): Measurement counts.
+        title (str): Title for the plot.
+    """
+    plt.bar(counts.keys(), counts.values())
+    plt.xticks(list(counts.keys()))
+    plt.xlabel("bitstrings")
+    plt.ylabel("counts")
+    plt.title(title)
---
+++ test_quantum_walk.py
@@ -0,0 +1,94 @@
+from braket.circuits import Circuit
+from braket.devices import LocalSimulator
+
+
+from quantum_walk import qft_conditional_add_1, quantum_walk
+import numpy as np
+import pytest
+
+
+def test_quantum_walk_4_nodes_4_steps():
+    local_simulator = LocalSimulator()
+    quantum_walk_circuit = quantum_walk(4, 4)
+    task = local_simulator.run(quantum_walk_circuit, shots=1000)
+    result = task.result()
+    counts = result.measurement_counts
+    
+    assert set(counts.keys()) == {'001'}
+
+    assert np.isclose(list(counts.values()), [1000]).all()
+
+
+def test_quantum_walk_4_nodes_1_step():
+    local_simulator = LocalSimulator()
+    quantum_walk_circuit = quantum_walk(4, 1)
+    shots = 10000
+    task = local_simulator.run(quantum_walk_circuit, shots=shots)
+    result = task.result()
+    counts = result.measurement_counts
+
+    assert set(counts.keys()) == {'010', '111'}
+
+    assert np.isclose(np.array(list(counts.values()))/shots , [0.5, 0.5], rtol=1e-02).all()
+
+
+@pytest.mark.parametrize("num_qubits, a", [(3, 0), (3, 1), (3, 2), (3, 3), (3, 4), (3, 5), (3, 6), (3, 7)])
+def test_qft_conditional_add_1(num_qubits, a):
+    qc = Circuit()
+    local_simulator = LocalSimulator()
+
+    # Step 0: Set the 0th qubit to 1 so that we are adding 1
+    qc.x(0)
+    # Step 1: Encode the number a into the circuit
+    binary_a = bin(abs(a))[2:]
+    for i, bit in enumerate(reversed(binary_a)):
+        if bit == '1':
+            qc.x(1+i)
+    
+    # Step 2: Apply the adder and get the results
+    qc.add_circuit(qft_conditional_add_1(num_qubits))
+    result = local_simulator.run(qc, shots=1000).result()
+    counts = result.measurement_counts
+    
+    # Step 3: Make sure the measurement outcome corresponds to the addition a+1
+    values = np.asarray([int(v[::-1], 2) for v in counts.keys()])
+    counts = np.asarray(list(counts.values()))
+    c = values[np.argmax(counts)]
+
+    assert np.mod(a+1-c, 2**num_qubits) == 0
+
+
+@pytest.mark.parametrize(
+    "num_qubits, a, action", 
+    [(3, 0, "+"), (3, 1, "+"), (3, 2, "+"), (3, 3, "+"), (3, 4, "+"), (3, 5, "+"), (3, 6, "+"), (3, 7, "+"),
+    (3, 0, "-"), (3, 1, "-"), (3, 2, "-"), (3, 3, "-"), (3, 4, "-"), (3, 5, "-"), (3, 6, "-"), (3, 7, "-"),
+    ]
+)
+def test_qft_conditional_minus_1(num_qubits, a, action):
+    qc = Circuit()
+    local_simulator = LocalSimulator()
+
+    # Step 0: Do nothing to the 0th qubit to 1 so that we are subtracting 1
+    if action == "-":
+        qc.x(0)
+    
+    # Step 1: Encode the number a into the circuit
+    binary_a = bin(abs(a))[2:]
+    for i, bit in enumerate(reversed(binary_a)):
+        if bit == '1':
+            qc.x(1+i)
+    
+    # Step 2: Add or subtract 1 conditionally
+    qc.add_circuit(qft_conditional_add_1(num_qubits))
+    result = local_simulator.run(qc, shots=1000).result()
+    counts = result.measurement_counts
+    
+    # Step 3: Make sure the measurement outcome corresponds to the addition a+b
+    values = np.asarray([int(v[1:][::-1], 2) for v in counts.keys()])
+    counts = np.asarray(list(counts.values()))
+    c = values[np.argmax(counts)]
+
+    if action == "-":
+        assert np.mod(a-1-c, 2**num_qubits) == 0
+    else: # action == "+"
+        assert np.mod(a+1-c, 2**num_qubits) == 0
