2ba87d7d67087de9a3148e99395ea63538fda84e
Milan <30416311+krneta@users.noreply.github.com>
2022-11-10 13:32:09 -0800

    updating dir structure per feedback.

--- test/unit_tests/braket/experimental/algorithms/my_algorithm/test_example.py
+++ test/unit_tests/braket/experimental/algorithms/my_algorithm/test_example.py
@@ -10,7 +10,7 @@
 # distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF
 # ANY KIND, either express or implied. See the License for the specific
 # language governing permissions and limitations under the License.
-from braket.algorithm.example import example_function
+from braket.experimental.algorithms.my_algorithm.example import example_function
 
 
 def test_creation():
