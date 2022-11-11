769c35d786a0499cf20363d523d5419f61efe5bd
Michael Abel <michaab@amazon.com>
2022-11-11 21:00:19 +0000

    quantum phase estimation cleanup

--- test/unit_tests/braket/experimental/algorithms/quantum_phase_estimation/test_quantum_phase_estimation.py
+++ test/unit_tests/braket/experimental/algorithms/quantum_phase_estimation/test_quantum_phase_estimation.py
@@ -11,21 +11,15 @@
 # ANY KIND, either express or implied. See the License for the specific
 # language governing permissions and limitations under the License.
 
-from braket.circuits import Circuit, circuit
+from braket.circuits import Circuit
 from braket.devices import LocalSimulator
 
 from braket.experimental.algorithms.quantum_phase_estimation import quantum_phase_estimation as qpe
 
 
-# controlled unitary gate definition
-@circuit.subroutine(register=True)
-def qpe_cnot_unitary(control_qubit, target_qubits):
-    return Circuit().cnot(control=control_qubit, target=target_qubits)
-
-
 # controlled unitary apply function
 def controlled_unitary_apply_cnot_func(qpe_circ, control_qubit, query_qubits):
-    qpe_circ.qpe_cnot_unitary(control_qubit, query_qubits)
+    qpe_circ.cnot(control_qubit, query_qubits)
 
 
 # CNOT controlled unitary with 2 precision qubits, and H gate query prep
@@ -51,16 +45,15 @@ def test_cnot_qpe_run_2_precision_qubits():
         qpe_circ, precision_qubits, query_qubits, LocalSimulator()
     )
 
-    print(
-        f"test_cnot_qpe_run_2_precision_qubits Results: \
-      {qpe.get_quantum_phase_estimation_results(result)}"
+    agg_result = qpe.get_quantum_phase_estimation_results(
+        result, precision_qubits, query_qubits, verbose=True
     )
 
     # validate excepted QPE output
-    assert result["measurement_counts"]["001"] > 300
-    assert result["measurement_counts"]["000"] > 300
-    assert len(result["measurement_counts"]) == 2
-    assert result["phases_decimal"] == [0.0]
+    assert agg_result["measurement_counts"]["001"] > 300
+    assert agg_result["measurement_counts"]["000"] > 300
+    assert len(agg_result["measurement_counts"]) == 2
+    assert agg_result["phases_decimal"] == [0.0]
 
 
 # 0 shots should result in no measurement
@@ -77,6 +70,7 @@ def test_0_shots():
         precision_qubits, query_qubits, controlled_unitary_apply_cnot_func
     )
 
+    print("Circuit: ", qpe_circ)
     assert len(qpe_circ.instructions) == 8
     assert qpe_circ.depth == 6
     assert qpe_circ.instructions[3].ascii_symbols == ("C", "X")
@@ -86,14 +80,13 @@ def test_0_shots():
         qpe_circ, precision_qubits, query_qubits, LocalSimulator(), shots=0
     )
 
-    print(
-        f"test_0_shots Results: \
-       {qpe.get_quantum_phase_estimation_results(result)}"
+    agg_result = qpe.get_quantum_phase_estimation_results(
+        result, precision_qubits, query_qubits, verbose=True
     )
 
     # validate excepted QPE output
-    assert not result["measurement_counts"]
-    assert not result["phases_decimal"]
+    assert not agg_result["measurement_counts"]
+    assert not agg_result["phases_decimal"]
 
 
 # CNOT controlled unitary with 3 precision qubits, and H gate query prep
@@ -110,21 +103,21 @@ def test_cnot_qpe_run_3_precision_qubits():
         precision_qubits, query_qubits, controlled_unitary_apply_cnot_func
     )
 
+    print("Circuit: ", qpe_circ)
     # run QPE
     result = qpe.run_quantum_phase_estimation(
         qpe_circ, precision_qubits, query_qubits, LocalSimulator()
     )
 
-    print(
-        f"test_cnot_qpe_run_3_precision_qubits Results: \
-       {qpe.get_quantum_phase_estimation_results(result)}"
+    agg_result = qpe.get_quantum_phase_estimation_results(
+        result, precision_qubits, query_qubits, verbose=True
     )
 
     # validate excepted QPE output
-    assert result["measurement_counts"]["0001"] > 300
-    assert result["measurement_counts"]["0000"] > 300
-    assert len(result["measurement_counts"]) == 2
-    assert result["phases_decimal"] == [0.0]
+    assert agg_result["measurement_counts"]["0001"] > 300
+    assert agg_result["measurement_counts"]["0000"] > 300
+    assert len(agg_result["measurement_counts"]) == 2
+    assert agg_result["phases_decimal"] == [0.0]
 
 
 # CNOT controlled unitary with 2 precision qubits, and HX gate query prep
@@ -141,21 +134,21 @@ def test_cnot_qpe_run_HX_eigenstate():
         precision_qubits, query_qubits, controlled_unitary_apply_cnot_func
     )
 
+    print("Circuit: ", qpe_circ)
     # run QPE
     result = qpe.run_quantum_phase_estimation(
         qpe_circ, precision_qubits, query_qubits, LocalSimulator()
     )
 
-    print(
-        f"test_cnot_qpe_run_HX_eigenstate Results: \
-       {qpe.get_quantum_phase_estimation_results(result)}"
+    agg_result = qpe.get_quantum_phase_estimation_results(
+        result, precision_qubits, query_qubits, verbose=True
     )
 
     # validate excepted QPE output
-    assert result["measurement_counts"]["101"] > 300
-    assert result["measurement_counts"]["100"] > 300
-    assert len(result["measurement_counts"]) == 2
-    assert result["phases_decimal"] == [0.5]
+    assert agg_result["measurement_counts"]["101"] > 300
+    assert agg_result["measurement_counts"]["100"] > 300
+    assert len(agg_result["measurement_counts"]) == 2
+    assert agg_result["phases_decimal"] == [0.5]
 
 
 # CNOT controlled unitary with 2 precision qubits, and X gate query prep
@@ -172,23 +165,23 @@ def test_cnot_qpe_run_X_eigenstate():
         precision_qubits, query_qubits, controlled_unitary_apply_cnot_func
     )
 
+    print("Circuit: ", qpe_circ)
     # run QPE
     result = qpe.run_quantum_phase_estimation(
         qpe_circ, precision_qubits, query_qubits, LocalSimulator()
     )
 
-    print(
-        f"test_cnot_qpe_run_HX_eigenstate Results: \
-       {qpe.get_quantum_phase_estimation_results(result)}"
+    agg_result = qpe.get_quantum_phase_estimation_results(
+        result, precision_qubits, query_qubits, verbose=True
     )
 
     # validate excepted QPE output
-    assert result["measurement_counts"]["000"] > 100
-    assert result["measurement_counts"]["100"] > 100
-    assert result["measurement_counts"]["101"] > 100
-    assert result["measurement_counts"]["001"] > 100
-    assert len(result["measurement_counts"]) == 4
-    assert set(result["phases_decimal"]) == {0.0, 0.5}
+    assert agg_result["measurement_counts"]["000"] > 100
+    assert agg_result["measurement_counts"]["100"] > 100
+    assert agg_result["measurement_counts"]["101"] > 100
+    assert agg_result["measurement_counts"]["001"] > 100
+    assert len(agg_result["measurement_counts"]) == 4
+    assert set(agg_result["phases_decimal"]) == {0.0, 0.5}
 
 
 # inverse QFT circuit validation with 2 input qubits
