d9353e6b57bbbe3d52a2d028bb48471b6f9964a6
Tim <yitchen@amazon.com>
2022-11-14 17:31:36 -0500

    fix test

--- test/unit_tests/braket/experimental/algorithms/grover_search/test_grover_search.py
+++ test/unit_tests/braket/experimental/algorithms/grover_search/test_grover_search.py
@@ -1,14 +1,14 @@
 import numpy as np
 from braket.devices import LocalSimulator
 
-from braket.experimental.algorithms.grover_search.grover_search import get_oracle, grover_search
+from braket.experimental.algorithms.grover_search.grover_search import build_oracle, grover_search
 
 
 def test_grover_search():
     solution = "000"
     n_qubits = len(solution)
 
-    oracle = get_oracle(solution)
+    oracle = build_oracle(solution)
     circuit = grover_search(oracle, n_qubits=n_qubits, n_reps=1)
 
     local_simulator = LocalSimulator()
