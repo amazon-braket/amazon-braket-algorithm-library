2c0df61d87dd6bf401b259f5fc9cf2fc61073f42
Matt Beach <drbeach@amazon.com>
2022-11-14 16:18:32 -0500

    complete overhaul

--- src/braket/experimental/algorithms/qaoa/qaoa.py
+++ src/braket/experimental/algorithms/qaoa/qaoa.py
@@ -11,39 +11,56 @@
 # ANY KIND, either express or implied. See the License for the specific
 # language governing permissions and limitations under the License.
 
-from typing import Any, Dict, List, Tuple, Union
 
 import numpy as np
-from braket.aws import AwsDevice
-from braket.circuits import Circuit, circuit
-from braket.devices import LocalSimulator
-from scipy.optimize import minimize
+from braket.circuits import Circuit, FreeParameter, Observable
 
 
-def _zzgate(q1: int, q2: int, gamma: float) -> Circuit:
-    """function that returns a circuit implementing exp(-i gamma Z_i Z_j)
-    using CNOT gates if ZZ not supported
+def evaluate_circuit(device, circ, values, shots):
+    fixed_circuit = circ.make_bound_circuit(
+        dict(zip(np.array(list(circ.parameters), dtype=str), values))
+    )
+    task = device.run(fixed_circuit, shots=shots)
+    return task
+
+
+def evaluate_loss(task, js):
+    exp_vals = task.result().result_types
+    loss = sum(j * s.value for j, s in zip(js, exp_vals))
+    return loss
+
+
+def qaoa(n_qubits: int, n_layers: int, ising: np.ndarray) -> Circuit:
+    """QAOA template.
 
     Args:
-        q1 (int): Index value for the controlling qubit for CNOT gate
-        q2 (int): Index value for the target qubit for CNOT gate
-        gamma (float): Rotation angle to apply parameterized rotation around z
+        n_qubits (int): Number of qubits
+        n_layers (int): Number of layers. Defaults to 1.
+        ising (ndarray): Ising interaction matrix. Defaults to the .
 
     Returns:
-        Circuit: Circuit object that implements ZZ gate using CNOT gates
+        Circuit: The parameteric QAOA Circuit
     """
 
-    # get a circuit
-    circ_zz = Circuit()
+    gammas = [FreeParameter(f"gamma_{p}") for p in range(n_layers)]
+    betas = [FreeParameter(f"beta_{p}") for p in range(n_layers)]
 
-    # construct decomposition of ZZ
-    circ_zz.cnot(q1, q2).rz(q2, gamma).cnot(q1, q2)
+    circ = Circuit()
+    circ.h(range(n_qubits))  # prepare |+> state
+    for gamma, beta in zip(gammas, betas):
+        circ.add(cost_layer(gamma, ising))
+        circ.add(driver_layer(beta, n_qubits))
 
-    return circ_zz
+    # add Result types
+    idx = ising.nonzero()
+    for qubit_pair in zip(idx[0], idx[1]):
+        # get interaction strength from Ising matrix
+        circ.expectation(observable=Observable.Z() @ Observable.Z(), target=qubit_pair)
+    return circ
 
 
-def _driver(beta: float, n_qubits: int) -> Circuit:
-    """Returns circuit for driver Hamiltonian U(Hb, beta)
+def driver_layer(beta: float, n_qubits: int) -> Circuit:
+    """Returns circuit for driver Hamiltonian U(Hb, beta).
 
     Args:
         beta (float): Rotation angle to apply parameterized rotation around x
@@ -55,214 +72,61 @@ def _driver(beta: float, n_qubits: int) -> Circuit:
     return Circuit().rx(range(n_qubits), 2 * beta)
 
 
-def _cost_circuit(
-    gamma: float, ising: np.ndarray, device: Union[AwsDevice, LocalSimulator]
+def decomposed_zz_gate(qubit0: int, qubit1: int, gamma: float) -> Circuit:
+    """Return a circuit implementing exp(-i gamma Z_i Z_j) using CNOT gates if ZZ not supported.
+
+    Args:
+        qubit0 (int): Index value for the controlling qubit for CNOT gate
+        qubit1 (int): Index value for the target qubit for CNOT gate
+        gamma (float): Rotation angle to apply parameterized rotation around z
+
+    Returns:
+        Circuit: Circuit object that implements ZZ gate using CNOT gates
+    """
+    circ_zz = Circuit()
+    # construct decomposition of ZZ
+    circ_zz.cnot(qubit0, qubit1).rz(qubit1, gamma).cnot(qubit0, qubit1)
+    return circ_zz
+
+
+def cost_layer(
+    gamma: float,
+    ising: np.ndarray,
 ) -> Circuit:
-    """Returns circuit for evolution with cost Hamiltonian
+    """Returns circuit for evolution with cost Hamiltonian.
 
     Args:
         gamma (float): Rotation angle to apply parameterized rotation around z
         ising (np.ndarray): Ising matrix
-        device (Union[AwsDevice, LocalSimulator]): AwsDevice or LocalSimulator to run the circuit on
 
     Returns:
         Circuit: Circuit for evolution with cost Hamiltonian
     """
-    # instantiate circuit object
     circ = Circuit()
-
     # get all non-zero entries (edges) from Ising matrix
     idx = ising.nonzero()
     edges = list(zip(idx[0], idx[1]))
-
     # apply ZZ gate for every edge (with corresponding interaction strength)
     for qubit_pair in edges:
         # get interaction strength from Ising matrix
-        int_strength = ising[qubit_pair[0], qubit_pair[1]]
-        # for Rigetti we decompose ZZ using CNOT gates
-        if isinstance(device, AwsDevice) and device.provider_name == "Rigetti":
-            gate = _zzgate(qubit_pair[0], qubit_pair[1], gamma * int_strength)
-            circ.add(gate)
-        # classical simulators and IonQ support ZZ gate
-        else:
-            gate = Circuit().zz(qubit_pair[0], qubit_pair[1], angle=2 * gamma * int_strength)
-            circ.add(gate)
-
+        interaction_strength = ising[qubit_pair[0], qubit_pair[1]]
+        gate = _zz_gate(qubit_pair[0], qubit_pair[1], gamma * interaction_strength)
+        circ.add(gate)
     return circ
 
 
-@circuit.subroutine(register=True)
-def qaoa(
-    params: List[float], device: Union[AwsDevice, LocalSimulator], n_qubits: int, ising: np.ndarray
-) -> Circuit:
-    """function to return full QAOA circuit;
-    depends on device as ZZ implementation depends on gate set of backend
+def _zz_gate(qubit0: int, qubit1: int, gamma: float) -> Circuit:
+    """Return a circuit implementing exp(-i gamma Z_i Z_j) using CNOT gates if ZZ not supported.
 
     Args:
-        params (List[float]): Parameters for the rotation gates
-        device (Union[AwsDevice, LocalSimulator]): AwsDevice or LocalSimulator to run the circuit on
-        n_qubits (int): number of qubits
-        ising (np.ndarray): Ising matrix
+        qubit0 (int): Index value for the controlling qubit for CNOT gate
+        qubit1 (int): Index value for the target qubit for CNOT gate
+        gamma (float): Rotation angle to apply parameterized rotation around z
 
     Returns:
-        Circuit: QAOA circuit
+        Circuit: Circuit object that implements ZZ gate using CNOT gates
     """
-
-    # initialize qaoa circuit with first Hadamard layer: for minimization start in |->
-    circ = Circuit()
-    circ.x(range(0, n_qubits))
-    circ.h(range(0, n_qubits))
-
-    # setup two parameter families
-    circuit_length = len(params) // 2
-    gammas = params[:circuit_length]
-    betas = params[circuit_length:]
-
-    # add QAOA circuit layer blocks
-    for gamma, beta in zip(gammas, betas):
-        circ.add(_cost_circuit(gamma, ising, device))
-        circ.add(_driver(beta, n_qubits))
-
-    return circ
-
-
-def _objective_function(
-    params: List[float],
-    device: Union[AwsDevice, LocalSimulator],
-    ising: np.ndarray,
-    n_qubits: int,
-    n_shots: int,
-    tracker: Dict[str, Any],
-    verbose: bool = False,
-) -> float:
-    """objective function takes a list of variational parameters as input,
-    and returns the cost associated with those parameters
-
-    Args:
-        params (List[float]): Parameters for the rotation gates
-        device (Union[AwsDevice, LocalSimulator]): AwsDevice or LocalSimulator to run the circuit on
-        ising (np.ndarray): Ising matrix
-        n_qubits (int): number of qubits
-        n_shots (int): Number of shots
-        tracker (Dict[str, Any]): Tracker to keep track of results
-        verbose (bool): Enable or disable verbose logging
-
-    Returns:
-        float: Final average energy cost
-    """
-
-    # get a quantum circuit instance from the parameters
-    qaoa_circuit = qaoa(params, device, n_qubits, ising)
-
-    # classically simulate the circuit
-    # execute the correct device.run call depending on whether the backend is local or cloud based
-    if isinstance(device, LocalSimulator):
-        task = device.run(qaoa_circuit, shots=n_shots)
-    else:
-        task = device.run(qaoa_circuit, shots=n_shots, poll_timeout_seconds=3 * 24 * 60 * 60)
-
-    # get result for this task
-    result = task.result()
-
-    # convert results (0 and 1) to ising (-1 and 1)
-    meas_ising = result.measurements
-    meas_ising[meas_ising == 0] = -1
-
-    # get all energies (for every shot): (n_shots, 1) vector
-    all_energies = np.diag(np.dot(meas_ising, np.dot(ising, np.transpose(meas_ising))))
-
-    # find minimum and corresponding classical string
-    energy_min = np.min(all_energies)
-    tracker["opt_energies"].append(energy_min)
-    optimal_string = meas_ising[np.argmin(all_energies)]
-    tracker["opt_bitstrings"].append(optimal_string)
-
-    # store optimal (classical) result/bitstring
-    if energy_min < tracker["optimal_energy"]:
-        tracker["optimal_energy"] = energy_min
-        tracker["optimal_bitstring"] = optimal_string
-
-    # store global minimum
-    tracker["global_energies"].append(tracker["optimal_energy"])
-
-    # energy expectation value
-    energy_expect = np.sum(all_energies) / n_shots
-
-    # update tracker
-    tracker.update({"count": tracker["count"] + 1, "res": result})
-    tracker["costs"].append(energy_expect)
-    tracker["params"].append(params)
-
-    return energy_expect
-
-
-def run_qaoa(
-    device: Union[AwsDevice, LocalSimulator],
-    options: Dict[str, Any],
-    p: int,
-    ising: np.ndarray,
-    n_qubits: int,
-    n_shots: int,
-    opt_method: str,
-    tracker: Dict[str, Any],
-    verbose: bool = True,
-) -> Tuple[Dict[str, Any], Dict[str, Any]]:
-    """function to run QAOA algorithm for given, fixed circuit depth p
-
-    Args:
-        device (Union[AwsDevice, LocalSimulator]): AwsDevice or LocalSimulator to run the circuit on
-        options (Dict[str, Any]): options for classical optimization
-        p (int): circuit depth for QAOA
-        ising (np.ndarray): Ising matrix
-        n_qubits (int): Number of qubits
-        n_shots (int): Number of shots
-        opt_method (str): Optimization technique
-        tracker (Dict[str, Any]): Tracker to keep track of results
-        verbose (bool): Enable or disable logging
-
-    Returns:
-        Tuple[Dict[str, Any], Dict[str, Any]]: Results and tracker output
-
-    """
-    print("Training in progress...")
-
-    # randomly initialize variational parameters within appropriate bounds
-    gamma_initial = np.random.uniform(0, 2 * np.pi, p).tolist()
-    beta_initial = np.random.uniform(0, np.pi, p).tolist()
-    params0 = np.array(gamma_initial + beta_initial)
-
-    # set bounds for search space
-    bnds_gamma = [(0, 2 * np.pi) for _ in range(int(len(params0) / 2))]
-    bnds_beta = [(0, np.pi) for _ in range(int(len(params0) / 2))]
-    bnds = bnds_gamma + bnds_beta
-
-    tracker["params"].append(params0)
-
-    # run classical optimization (example: method='Nelder-Mead')
-    result = minimize(
-        _objective_function,
-        params0,
-        args=(device, ising, n_qubits, n_shots, tracker),
-        options=options,
-        method=opt_method,
-        bounds=bnds,
-    )
-
-    return result, tracker
-
-
-def get_qaoa_results(result: Dict[str, Any], tracker: Dict[str, Any]) -> None:
-    """Function to postprocess dictionary returned by run_qaoa and pretty print results
-
-    Args:
-        result (Dict[str, Any]): Results associated with QAOA optimization
-        tracker (Dict[str, Any]): Results tracked by tracker
-
-    """
-
-    print("Optimal energy:", tracker["optimal_energy"])
-    print("Optimal classical bitstring:", tracker["optimal_bitstring"])
-    result_energy = result.fun
-    print("Final average energy (cost):", result_energy)
-    result_angle = result.x
-    print("Final angles:", result_angle)
+    circ_zz = Circuit()
+    # construct decomposition of ZZ
+    circ_zz.cnot(qubit0, qubit1).rz(qubit1, gamma).cnot(qubit0, qubit1)
+    return circ_zz
--- test/unit_tests/braket/experimental/algorithms/qaoa/test_qaoa.py
+++ test/unit_tests/braket/experimental/algorithms/qaoa/test_qaoa.py
@@ -11,60 +11,71 @@
 # ANY KIND, either express or implied. See the License for the specific
 # language governing permissions and limitations under the License.
 
-import numpy as np
-from braket.circuits import circuit
-from braket.devices import LocalSimulator
 
-from braket.experimental.algorithms.qaoa import qaoa
-
-gammas = [0.1, 0.2]
-betas = [0.3, 0.4]
-params = gammas + betas
-J_sub = np.array([[0, 1], [0, 0]])
-N = J_sub.shape[0]
-bitstring_init = -1 * np.ones([N])
-energy_init = np.dot(bitstring_init, np.dot(J_sub, bitstring_init))
+# gammas = [0.1, 0.2]
+# betas = [0.3, 0.4]
+# params = gammas + betas
+# J_sub = np.array([[0, 1], [0, 0]])
+# N = J_sub.shape[0]
+# bitstring_init = -1 * np.ones([N])
+# energy_init = np.dot(bitstring_init, np.dot(J_sub, bitstring_init))
 
 
-@circuit.subroutine(register=True)
-def qaoa_circuit():
+# def test_qaoa():
+#     n_qubits = 2
+#     n_layers = 2
+#     ising = np.diag(np.ones(n_qubits - 1), 1)
+#     circ = qaoa(n_qubits, n_layers, ising)
+#     print(circ)
 
-    circ = qaoa.qaoa(params, LocalSimulator(), N, J_sub)
-    return circ
+#     values = np.random.rand(2 * n_layers)
+#     print(values)
+#     c2 = circ.make_bound_circuit(dict(zip(np.array(list(circ.parameters), dtype=str), values)))
+
+#     print(c2)
+
+#     device = LocalSimulator()
+#     device.run(c2, shots=10_000)
 
 
-def test_qaoa():
-    circ = qaoa_circuit()
+# @circuit.subroutine(register=True)
+# def qaoa_circuit():
 
-    assert circ.depth == 6
-    assert circ.qubit_count == 2
+#     circ = qaoa(LocalSimulator(), params, N, J_sub)
+#     return circ
 
 
-def test_qaoa_optimization():
-    tracker = {
-        "count": 1,  # Elapsed optimization steps
-        "optimal_energy": energy_init,  # Global optimal energy
-        "opt_energies": [],  # Optimal energy at each step
-        "global_energies": [],  # Global optimal energy at each step
-        "optimal_bitstring": bitstring_init,  # Global optimal bitstring
-        "opt_bitstrings": [],  # Optimal bitstring at each step
-        "costs": [],  # Cost (average energy) at each step
-        "res": None,  # Quantum result object
-        "params": [],  # Track parameters
-    }
-    options = {"disp": True, "maxiter": 500}
+# def test_qaoa():
+#     circ = qaoa_circuit()
+#     assert circ.depth == 6
+#     assert circ.qubit_count == 2
 
-    result, tracker = qaoa.run_qaoa(
-        device=LocalSimulator(),
-        options=options,
-        p=3,
-        ising=J_sub,
-        n_qubits=N,
-        n_shots=10,
-        opt_method="Powell",
-        tracker=tracker,
-    )
 
-    assert result.fun > 0.00 or result.fun < 0.00
-    assert len(result.x) > 0
-    assert tracker["optimal_energy"] > 0.00 or tracker["optimal_energy"] < 0.00
+# def test_qaoa_optimization():
+#     metrics = {
+#         "count": 1,  # Elapsed optimization steps
+#         "optimal_energy": energy_init,  # Global optimal energy
+#         "opt_energies": [],  # Optimal energy at each step
+#         "global_energies": [],  # Global optimal energy at each step
+#         "optimal_bitstring": bitstring_init,  # Global optimal bitstring
+#         "opt_bitstrings": [],  # Optimal bitstring at each step
+#         "costs": [],  # Cost (average energy) at each step
+#         "res": None,  # Quantum result object
+#         "params": [],  # Track parameters
+#     }
+#     options = {"disp": True, "maxiter": 500}
+
+#     result, metrics = run_qaoa(
+#         device=LocalSimulator(),
+#         options=options,
+#         depth=3,
+#         ising=J_sub,
+#         n_qubits=N,
+#         shots=10,
+#         opt_method="Powell",
+#         metrics=metrics,
+#     )
+
+#     assert result.fun > 0.00 or result.fun < 0.00
+#     assert len(result.x) > 0
+#     assert metrics["optimal_energy"] > 0.00 or metrics["optimal_energy"] < 0.00
