451b2cfd37364bca3e6cda4cc2b8ea0ec905ec9e
Jean-Christophe Jaskula <jjaskula@amazon.com>
2022-11-15 16:56:45 -0500

    Fix comments

--- src/braket/experimental/algorithms/simons_algorithm/simons_algorithm.py
+++ src/braket/experimental/algorithms/simons_algorithm/simons_algorithm.py
@@ -25,9 +25,14 @@ from sympy import Matrix
 def simons_oracle(secret_string: str) -> Circuit:
     """Quantum circuit implementing a particular oracle for Simon's problem.
 
-    Details of this
-    implementation are explained in an example notebook readable in the Amazon Braket example
-    https://github.com/aws/amazon-braket-examples/blob/main/examples/advanced_circuits_algorithms/Simons_Algorithm/Simons_Algorithm.ipynb
+    In the quantum setting, we first copy the input register into some
+    ancillary qubits: |x>|0> -> |x>|x>.
+
+    We then perform the quantum analog of XOR, which means we apply an X gate
+    to the kth qubit whenever the kth bit of `string` is 1. However, we only
+    apply this X gate when the flag qubit is also |1>. Thus, our X gate becomes
+    a CNOT gate between the flag qubit on the input register, and the kth qubit
+    on the output.
 
     Args:
         secret_string (str): the secret string
@@ -35,7 +40,7 @@ def simons_oracle(secret_string: str) -> Circuit:
     Returns:
         Circuit: Circuit object that implements the oracle
     """
-    # Find the index of the first 1 in s, to be used as the flag bit
+    # Find the index of the first 1 in secret_string, to be used as the flag bit
     flag_bit = secret_string.find("1")
 
     length_string = len(secret_string)
@@ -45,19 +50,19 @@ def simons_oracle(secret_string: str) -> Circuit:
     for i in range(length_string):
         circ.cnot(i, i + length_string)
 
-    # If flag_bit=-1, s is the all-zeros string, and we do nothing else.
+    # If flag_bit=-1, secret_string is the all-zeros string, and we do nothing else.
     if flag_bit != -1:
-        # Now apply the XOR with s whenever the flag bit is 1.
+        # Now apply the XOR with secret_string whenever the flag bit is 1.
         for index, bit_value in enumerate(secret_string):
 
             if bit_value not in ["0", "1"]:
                 raise ValueError(
-                    "Incorrect char '" + bit_value + "' in secret string s:" + secret_string
+                    "Incorrect char '" + bit_value + "' in secret string secret_string:" + secret_string
                 )
 
-            # XOR with s whenever the flag bit is 1.
+            # XOR with secret_string whenever the flag bit is 1.
             # In terms of gates, XOR means we apply an X gate only
-            # whenever the corresponding bit in s is 1.
+            # whenever the corresponding bit in secret_string is 1.
             # Applying this X only when the flag qubit is 1 means this is a CNOT gate.
             if bit_value == "1":
                 circ.cnot(flag_bit, index + length_string)
