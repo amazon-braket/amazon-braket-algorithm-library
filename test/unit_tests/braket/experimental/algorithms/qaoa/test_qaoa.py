b005526d7cae2a54c1f1b5e1c48d5f1aeccb1f25
Tim <yitchen@amazon.com>
2022-11-15 01:05:06 -0500

    add test

--- test/unit_tests/braket/experimental/algorithms/qaoa/test_qaoa.py
+++ test/unit_tests/braket/experimental/algorithms/qaoa/test_qaoa.py
@@ -11,6 +11,47 @@
 # ANY KIND, either express or implied. See the License for the specific
 # language governing permissions and limitations under the License.
 
+import numpy as np
+from braket.circuits import Circuit, FreeParameter, Observable
+from braket.devices import LocalSimulator
+
+from braket.experimental.algorithms.qaoa.qaoa import (
+    qaoa, evaluate_circuit, evaluate_loss
+)
+
+
+def test_qaoa():
+    n_qubits = 2
+    n_layers = 1
+    coupling_matrix = np.diag(np.ones(n_qubits - 1), 1)
+    circ = qaoa(n_qubits, n_layers, coupling_matrix)
+    assert circ.qubit_count == 2
+
+
+def test_qaoa_evaluate_circuit():
+    param = FreeParameter("theta")
+    circ = Circuit().rx(0, param).probability()
+    device = LocalSimulator()
+    shots = 0
+    values = [0]
+    task = evaluate_circuit(device, circ, values, shots)
+    result = task.result().values[0]
+    assert np.isclose(result[0], 1)
+
+
+def test_qaoa_evaluate_loss():
+    circ = Circuit().i(0)
+    circ.expectation(observable=Observable.Z(), target=0)
+    task = LocalSimulator().run(circ, shots=0)
+    js = [1]
+    assert np.isclose(evaluate_loss(task, js), 1)
+    
+
+
+
+
+
+
 
 # gammas = [0.1, 0.2]
 # betas = [0.3, 0.4]
@@ -79,3 +120,7 @@
 #     assert result.fun > 0.00 or result.fun < 0.00
 #     assert len(result.x) > 0
 #     assert metrics["optimal_energy"] > 0.00 or metrics["optimal_energy"] < 0.00
+
+
+
+
