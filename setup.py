91a6fde95e69bbc1a9825b0e633c17ce5c6b01e3
Tim <yitchen@amazon.com>
2022-11-18 17:52:26 -0500

    add dependencies

--- setup.py
+++ setup.py
@@ -26,7 +26,15 @@ setup(
     python_requires=">= 3.7.2",
     packages=find_namespace_packages(where="src", exclude=("test",)),
     package_dir={"": "src"},
-    install_requires=["amazon-braket-sdk", "matplotlib<3.6", "sympy", "scipy>=1.5.3", "pyscf>=2.1.1"],
+    install_requires=[
+        "amazon-braket-sdk", 
+        "matplotlib<3.6", 
+        "sympy", 
+        "scipy>=1.5.3", 
+        "pyscf>=2.1.1", 
+        "pennylane",
+        "openfermion==1.0.0",
+        ],
     extras_require={
         "test": [
             "black>=22.3.0",
