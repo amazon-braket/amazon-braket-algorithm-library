03aa8acd65a8ebc26d65fd35c7ebbd573b6f2873
Tim <yitchen@amazon.com>
2022-11-18 17:28:33 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ setup(
     python_requires=">= 3.7.2",
     packages=find_namespace_packages(where="src", exclude=("test",)),
     package_dir={"": "src"},
-    install_requires=["amazon-braket-sdk", "matplotlib<3.6", "sympy"],
+    install_requires=["amazon-braket-sdk", "matplotlib<3.6", "sympy", "scipy>=1.5.3", "pyacf>=2.2.1"],
     extras_require={
         "test": [
             "black>=22.3.0",
