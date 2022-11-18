e72b6c7ec99304ac4b4da280bfb1ed8ed9ec2337
Tim <yitchen@amazon.com>
2022-11-18 18:06:17 -0500

    remove whitespace

--- setup.py
+++ setup.py
@@ -27,14 +27,14 @@ setup(
     packages=find_namespace_packages(where="src", exclude=("test",)),
     package_dir={"": "src"},
     install_requires=[
-        "amazon-braket-sdk", 
-        "matplotlib<3.6", 
-        "sympy", 
-        "scipy>=1.5.3", 
-        "pyscf>=2.1.1", 
+        "amazon-braket-sdk",
+        "matplotlib<3.6",
+        "sympy",
+        "scipy>=1.5.3",
+        "pyscf>=2.1.1",
         "pennylane",
         "openfermion==1.0.0",
-        ],
+    ],
     extras_require={
         "test": [
             "black>=22.3.0",
