b4828a3da637447e7ab08906368a378f4402ae88
Tim <yitchen@amazon.com>
2022-12-06 10:08:07 -0500

    remove import

--- src/braket/experimental/algorithms/qc_qmc/qc_qmc.py
+++ src/braket/experimental/algorithms/qc_qmc/qc_qmc.py
@@ -5,7 +5,8 @@ from typing import Callable, List, Tuple
 import numpy as np
 import pennylane as qml
 from openfermion.linalg.givens_rotations import givens_decomposition_square
-from scipy.linalg import expm
+
+# from scipy.linalg import expm
 
 from braket.experimental.algorithms.qc_qmc.classical_afqmc import (
     ChemicalProperties,
