695b12c8635e5ec45db860968345187bcdb112ff
Tim <yitchen@amazon.com>
2022-12-23 16:50:11 -0500

    clean up commented lines

--- src/braket/experimental/algorithms/qc_qmc/__init__.py
+++ src/braket/experimental/algorithms/qc_qmc/__init__.py
@@ -10,18 +10,3 @@
 # distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF
 # ANY KIND, either express or implied. See the License for the specific
 # language governing permissions and limitations under the License.
-
-# from braket.experimental.algorithms.afqmc.quantum_afqmc import (  # noqa: F401
-#     prepare_slater_circuit,
-#     reortho,
-# )
-
-# from braket.experimental.algorithms.afqmc.classical_afqmc import (
-#     ChemicalProperties,
-#     greens_pq,
-#     hartree_fock_energy,
-#     imag_time_propogator,
-#     local_energy,
-#     propagate_walker,
-#     reortho,
-# )
