581573a77cbc9c40d8b2aac381a57bf8c0bd6c4c
yuttaky <yuttaky@amazon.co.jp>
2023-03-31 06:46:53 +0000

    second commit

--- notebooks/textbook/Quantum_Neuron/qn/quantum_neuron.py
+++ notebooks/textbook/Quantum_Neuron/qn/quantum_neuron.py
@@ -1,3 +1,16 @@
+# Copyright Amazon.com Inc. or its affiliates. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"). You
+# may not use this file except in compliance with the License. A copy of
+# the License is located at
+#
+#     http://aws.amazon.com/apache2.0/
+#
+# or in the "license" file accompanying this file. This file is
+# distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF
+# ANY KIND, either express or implied. See the License for the specific
+# language governing permissions and limitations under the License.
+
 import json
 import os
 import time
@@ -77,7 +90,7 @@ def quantum_neuron(inputs:str, weights, bias, n_qubits, dev):
         
         return [qml.sample(qml.PauliZ(i)) for i in range(n_qubits)]
     
-    # post-processing
+    ### start of post-processing ###
     sample = af_circuit()
     sample = sample.T
     sample = (1 - sample.numpy()) / 2
@@ -90,6 +103,7 @@ def quantum_neuron(inputs:str, weights, bias, n_qubits, dev):
     p_0 = count_0 / (count_0 + count_1)
     
     q_theta = np.arccos(np.sqrt(p_0))
+    ### end of post-processing ###
     
     return theta, q_theta
 
