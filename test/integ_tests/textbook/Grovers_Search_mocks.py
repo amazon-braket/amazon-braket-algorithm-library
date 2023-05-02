7ed80e0fa3465ec7ca3062b165bbc81818580c6d
Viraj Chaudhari <72896239+virajvchaudhari@users.noreply.github.com>
2023-05-02 12:24:16 -0700

    infra: add mock-integration tests

---
+++ test/integ_tests/textbook/Grovers_Search_mocks.py
@@ -0,0 +1,8 @@
+def pre_run_inject(mock_utils):
+    mocker = mock_utils.Mocker()
+    mock_utils.mock_default_device_calls(mocker)
+    mocker.set_task_result_return(mock_utils.read_file("grovers_results.json", __file__))
+
+
+def post_run(tb):
+    pass
