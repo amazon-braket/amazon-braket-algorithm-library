82f379a0dfefaa920a876673c4f2cadaeb38c869
YutoTakaki <yuttaky@amazon.co.jp>
2023-07-28 01:56:11 +0000

    add Quantum_Neuron_mocks.py

--- test/integ_tests/textbook/Quantum_Neuron_mocks.py
+++ test/integ_tests/textbook/Quantum_Neuron_mocks.py
@@ -1,9 +1,7 @@
 def pre_run_inject(mock_utils):
     mocker = mock_utils.Mocker()
     mock_utils.mock_default_device_calls(mocker)
-    mocker.set_task_result_return(
-        mock_utils.read_file("quantum_neuron_results.json", __file__)
-    )
+    mocker.set_task_result_return(mock_utils.read_file("quantum_neuron_results.json", __file__))
 
 
 def post_run(tb):
