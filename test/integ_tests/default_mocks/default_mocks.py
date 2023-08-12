b179e6c47c18d5d071258170ad5bc97ca9919f21
YutoTakaki <yuttaky@amazon.co.jp>
2023-08-12 06:39:59 +0000

    add mock for Quantum_Neuron

--- test/integ_tests/default_mocks/default_mocks.py
+++ test/integ_tests/default_mocks/default_mocks.py
@@ -1,14 +1,12 @@
 def pre_run_inject(mock_utils):
     print("This is inside default_mocks.py")
-    
+
     mocker = mock_utils.Mocker()
     mock_utils.mock_default_device_calls(mocker)
-    mocker.set_get_default_jobs_role('arn:aws:iam::000000:role/service-role/AmazonBraketJobsExecutionRole')
-    mocker.set_create_job_result(
-        {
-            "jobArn": "arn:aws:braket:us-east-1:000000:job/TestARN"
-        }
+    mocker.set_get_default_jobs_role(
+        "arn:aws:iam::000000:role/service-role/AmazonBraketJobsExecutionRole"
     )
+    mocker.set_create_job_result({"jobArn": "arn:aws:braket:us-east-1:000000:job/TestARN"})
 
 
 def post_run(tb):
