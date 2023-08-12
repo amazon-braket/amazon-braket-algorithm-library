31791736a7732a9dbf50af7305d7cacd65b87f25
YutoTakaki <yuttaky@amazon.co.jp>
2023-08-12 02:59:33 +0000

    add mock for Quantum_Neuron

--- test/integ_tests/mock_utils.py
+++ test/integ_tests/mock_utils.py
@@ -27,6 +27,10 @@ class Mocker:
 
     def set_create_quantum_task_result(self, result):
         self._wrapper.boto_client.create_quantum_task.return_value = result
+        
+    ### test
+    def set_create_job_result(self, result):
+        self._wrapper.boto_client.create_job.return_value = result
 
     def set_get_quantum_task_result(self, result):
         self._wrapper.boto_client.get_quantum_task.return_value = result
