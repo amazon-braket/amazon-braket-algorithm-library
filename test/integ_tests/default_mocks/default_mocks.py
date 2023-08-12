b782a5990cd18bbd97b113e897034687619fde4f
YutoTakaki <yuttaky@amazon.co.jp>
2023-08-12 03:33:42 +0000

    add mock for Quantum_Neuron

--- test/integ_tests/default_mocks/default_mocks.py
+++ test/integ_tests/default_mocks/default_mocks.py
@@ -1,6 +1,9 @@
 def pre_run_inject(mock_utils):
+    print("This is inside default_mocks.py")
+
     mocker = mock_utils.Mocker()
     mock_utils.mock_default_device_calls(mocker)
+    mocker.set_create_job_result({"jobArn": "arn:aws:braket:us-east-1:000000:job/TestARN"})
 
 
 def post_run(tb):
--- test/integ_tests/textbook/Quantum_Neuron_mocks.py
+++ test/integ_tests/textbook/Quantum_Neuron_mocks.py
@@ -1,4 +1,6 @@
 def pre_run_inject(mock_utils):
+    print("This is inside Quantum_Neuron_mocks.py")
+
     mocker = mock_utils.Mocker()
     mock_utils.mock_default_device_calls(mocker)
     mocker.set_create_job_result({"jobArn": "arn:aws:braket:us-east-1:000000:job/TestARN"})
