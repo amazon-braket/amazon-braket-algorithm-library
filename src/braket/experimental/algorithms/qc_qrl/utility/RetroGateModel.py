100ba387817c21948b501bc1b9b4dd4fab6dc62a
AoyuQC <aoyuzhang1989@gmail.com>
2024-01-12 14:10:40 +0000

    update to lightning.gpu

--- src/braket/experimental/algorithms/qc_qrl/utility/RetroGateModel.py
+++ src/braket/experimental/algorithms/qc_qrl/utility/RetroGateModel.py
@@ -160,7 +160,7 @@ class CirModel(nn.Module):
         optimizer = None
         if self.device == 'local':
             # dev = qml.device("braket.local.qubit", wires=self.n_qubits)
-            dev = qml.device("lightning.qubit", wires=self.n_qubits)
+            dev = qml.device("lightning.gpu", wires=self.n_qubits)
         elif self.device == 'sv1':
             dev = qml.device("braket.aws.qubit", 
             device_arn="arn:aws:braket:::device/quantum-simulator/amazon/sv1", 
