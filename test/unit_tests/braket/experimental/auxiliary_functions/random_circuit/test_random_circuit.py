711c3420caaaa12d45a09d105c765dbb3e442536
Yaroslav Kharkov <ykharkov@amazon.com>
2024-03-29 13:11:09 -0400

    Increase code coverage

--- test/unit_tests/braket/experimental/auxiliary_functions/random_circuit/test_random_circuit.py
+++ test/unit_tests/braket/experimental/auxiliary_functions/random_circuit/test_random_circuit.py
@@ -1,4 +1,5 @@
 from braket.circuits import Circuit
+from braket.circuits.gates import XY, CNot, CPhaseShift, H, Rx, Ry, Rz, S, T
 
 from braket.experimental.auxiliary_functions.random_circuit import random_circuit
 
@@ -24,3 +25,15 @@ def test_random_circuit_variability_without_seed():
     circuit1 = random_circuit(num_qubits=3, num_gates=5)
     circuit2 = random_circuit(num_qubits=3, num_gates=5)
     assert circuit1 != circuit2
+
+
+def test_custom_gate_set():
+    gate_set = [CNot, H, S, T, Rx, Ry, Rz, XY, CPhaseShift]
+    circuit = random_circuit(num_qubits=3, num_gates=5, gate_set=gate_set, seed=1)
+
+    gate_from_gate_set = []
+    for instr in circuit.instructions:
+        gate_class = instr.operator.__class__
+        gate_from_gate_set.append(gate_class in gate_set)
+
+    assert all(gate_from_gate_set)
